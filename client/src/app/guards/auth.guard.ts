603da54b4ce751d616ca044edc4ebff1a09811f5
Dmytro Fedorenko <fdaleks2015@gmail.com>
2024-06-20 14:13:42 +0300

    last commit

---
+++ client/src/app/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AccountService } from '../services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const accountService = inject(AccountService);
+  const toastr = inject(ToastrService);
+
+  const result = accountService.currentUser$.pipe(
+    map(user => {
+      if (user) 
+        return true;
+      else {
+        toastr.error('Authentication is required.');
+        return false;
+      }
+
+    })
+  );
+
+  return result;
+};
